abe6d9aa17480a3da00eea819155d79f6c008890
6arcia_Lui5 <luisgarciavv7@gmail.com>
2026-04-18 14:04:52 -0700

    Created table schema.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const pool = require('../db');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* DB test */
+router.get('/db-test', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('DB error');
+  }
 });
 
 module.exports = router;
